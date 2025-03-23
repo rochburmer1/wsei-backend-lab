e259729261ec11db8b109f13d4e93bf2d150279f
rochburmer1 <roch.burmer@microsoft.wsei.edu.pl>
2025-03-23 23:31:44 +0100

    webapi

---
+++ WebAPI/Program.cs
@@ -0,0 +1,57 @@
+using ApplicationCore.Commons.Repository;
+using ApplicationCore.Models;
+using ApplicationCore.Models.QuizAggregate;
+using BackendLab01;
+using Infrastructure.Memory.Generators;
+using Infrastructure.Memory.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddRazorPages();
+
+builder.Services.AddSingleton<IGenericRepository<Quiz, int>, MemoryGenericRepository<Quiz, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItem, int>, MemoryGenericRepository<QuizItem, int>>();
+builder.Services.AddSingleton<IGenericRepository<QuizItemUserAnswer, string>, MemoryGenericRepository<QuizItemUserAnswer, string>>();
+builder.Services.AddSingleton<IQuizAdminService, QuizAdminService>();
+builder.Services.AddSingleton<IQuizUserService, QuizUserService>();
+builder.Services.AddTransient<IGenericGenerator<int>, IntGenerator>();
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
